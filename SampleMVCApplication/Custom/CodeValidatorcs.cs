a229968e6e96ad864e83dd7ae844b4545b470469
vsr78 <vijenders.singh@gmail.com>
2024-12-09 16:16:37 -0500

    This is my first commit

---
+++ SampleMVCApplication/Custom/CodeValidatorcs.cs
@@ -0,0 +1,17 @@
+﻿namespace SampleMVCApplication.Custom
+{
+    using System.ComponentModel.DataAnnotations;
+    public class CodeValidatorcs : ValidationAttribute
+    {
+        protected override ValidationResult? IsValid(object? value, ValidationContext validationContext)
+        {
+            string code = value as string;
+            if (code != null && !code.StartsWith("P", StringComparison.OrdinalIgnoreCase))
+            {
+                return new ValidationResult(ErrorMessage);
+            }
+            return ValidationResult.Success;
+        }
+
+    }
+}
---
+++ SampleMVCApplication/Custom/MyCustomTagHelper.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+using Microsoft.DotNet.Scaffolding.Shared.Messaging;
+
+namespace SampleMVCApplication.Custom
+{
+    [HtmlTargetElement("custom")]
+    public class MyCustomTagHelper : TagHelper
+    {
+        public string Message { get; set; }
+
+        public ModelExpression AspFor { get; set; }
+       
+        //public override void Process(TagHelperContext context, TagHelperOutput output)
+        //{
+        //    output.TagName = "ui";
+        //    output.Content.AppendHtml($"<li>{Message}</li>");
+        //}
+
+        public override async Task  ProcessAsync(TagHelperContext context, TagHelperOutput output)
+        {
+            output.TagName = "ui";
+            var tagHelperContent = await output.GetChildContentAsync();
+            var content = tagHelperContent.GetContent();
+            var listItems = content.Split(",", StringSplitOptions.RemoveEmptyEntries);
+            foreach (var item in listItems)
+            {
+                output.Content.AppendHtml($"<li>{item}</li>");
+            } 
+        }
+    }
+}
